1cefd5048d9c1579386782c73ed2d686435f89c6
jdbenson <jdbenson@us.ibm.com>
2018-12-04 21:59:50 +0000

    Initial Files for PVC VM

---
+++ PVC-UBUNTU-VM/README.md
@@ -0,0 +1,8 @@
+## PVC-UBUNTU-VM infrastructure
+
+Created 12/4/18 by admin. for PVC-UBUNTU-VM
+
+
+## Design Decisions
+| Design item                | Decision|
+| :----------------------------------- | :--------------------------------------------------------------------------------|
---
+++ PVC-UBUNTU-VM/variables.tf
@@ -0,0 +1,61 @@
+#####################################################################
+##
+##      Created 12/4/18 by admin. for PVC-UBUNTU-VM
+##
+#####################################################################
+
+variable "openstack_user_name" {
+  type = "string"
+  description = "Generated"
+}
+
+variable "openstack_password" {
+  type = "string"
+  description = "Generated"
+}
+
+variable "openstack_project_name" {
+  type = "string"
+  description = "Generated"
+}
+
+variable "openstack_domain_name" {
+  type = "string"
+  description = "Generated"
+}
+
+variable "openstack_auth_url" {
+  type = "string"
+  description = "Generated"
+}
+
+variable "openstack_region" {
+  type = "string"
+  description = "Generated"
+}
+
+variable "instance_name" {
+  type = "string"
+  description = "Generated"
+}
+
+variable "openstack_image_name" {
+  type = "string"
+  description = "Generated"
+}
+
+variable "openstack_flavor_name" {
+  type = "string"
+  description = "Generated"
+}
+
+variable "openstack_key_pair_name" {
+  type = "string"
+  description = "Generated"
+}
+
+variable "openstack_network_id" {
+  type = "string"
+  description = "Generated"
+}
+
